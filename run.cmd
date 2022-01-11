b04d743b627c033b56c5b854e38b505f43ee61dc
rokon <rokon4eg@gmail.com>
2022-01-11 18:08:20 +0300

    changed file_name

--- run.cmd
+++ run.cmd
@@ -1,5 +1,9 @@
-python excludeip.py ppp_secret_from_cm.txt ip_from_address_plan.txt > 1_ip_exclude_address_plan.txt
-python excludeip.py 1_ip_exclude_address_plan.txt ppp_active_from_cm.txt > 2_ip_exclude_active.txt
-python excludeip.py 2_ip_exclude_active.txt eoip.txt > 3_ip_exclude_eoip.txt
-python excludeip.py eoip.txt ip_addr_from_cm.txt > eoip_exclude_ip_addr.txt
-python excludeip.py eoip_exclude_ip_addr.txt ppp_secret_from_cm.txt > 4_eoip_exclude_ip+secret.txt
+python excludeip.py ppp_secret_from_cm.txt ip_from_address_plan.txt > ppp_exclude_address_plan.txt
+python excludeip.py ppp_exclude_address_plan.txt ppp_active_from_cm.txt > ppp_exclude_address_plan_and_active.txt
+python excludeip.py ppp_exclude_address_plan_and_active.txt eoip.txt > 1_ppp_exclude_eoip.txt
+
+python excludeip.py eoip.txt ip_addr_from_cm.txt --local > 2_eoip_local_exclude_ip_addr_from_cm.txt
+
+python excludeip.py eoip.txt ip_from_address_plan.txt --remote > eoip_remote_exclude_ip_from_address_plan.txt
+python excludeip.py eoip_remote_exclude_ip_from_address_plan.txt ppp_active_from_cm.txt > 3_eoip_remote_exclude_address_plan_and_active.txt
+
