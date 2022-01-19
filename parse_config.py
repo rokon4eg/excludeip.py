08ea7f6941d8e167e0fb454611f30047ab26752d
rokon4eg <32700236+rokon4eg@users.noreply.github.com>
2022-01-19 13:33:04 +0300

    get_free_vlans

--- parse_config.py
+++ parse_config.py
@@ -3,7 +3,6 @@ from pprint import pprint
 from br_port_check import bridge_param
 from regex_example import parse_section, regex_section
 
-
 f'''
 1. Сравнить IP адреса из PPP secrets с адресами в ТУ (ip_from_address_plan.txt)
 Исключить активные PPP (ppp_active_from_cm.txt)
@@ -25,8 +24,9 @@ Ex.: python parse_config.py export_compact.txt.rsc [-tu ip_from_address_plan.txt
 
 
 def get_ip_secrets(config, file_tu, file_active=None):
-    """1. Сравнить IP адреса из PPP secrets с адресами в ТУ (ip_from_address_plan.txt)
-    Исключить активные PPP (ppp_active_from_cm.txt)
+    """
+ToDo: Сравнить IP адреса из PPP secrets с адресами в ТУ (ip_from_address_plan.txt)
+      Исключить активные PPP (ppp_active_from_cm.txt)
     """
     res = set()
 
@@ -38,12 +38,13 @@ def get_ip_secrets(config, file_tu, file_active=None):
 
     return res
 
+
 def get_ip_eoip(config, file_tu, file_active=None):
     """
-2. Исключить те EOIP для которых local-addresses участвуют в "ip addresses"
+2. ToDo: Исключить те EOIP для которых local-addresses участвуют в "ip addresses"
 
-3. Исключить те EOIP для которых remote-addresses есть в ТУ (ip_from_address_plan.txt)
-Исключить активные PPP (ppp_active_from_cm.txt)
+3. ToDo : Исключить те EOIP для которых remote-addresses есть в ТУ (ip_from_address_plan.txt)
+    Исключить активные PPP (ppp_active_from_cm.txt)
     """
     res = set()
 
@@ -58,30 +59,80 @@ def get_ip_eoip(config, file_tu, file_active=None):
 
     return res
 
-def get_bridges(config, file_tu):
-    """
-4. Исключить бриджы которые участвуют в "ip addresses"
-Вывести бриджы без портов
-Вывести бриджы с одним портом и эти одиночные порты
-    """
-    res = set()
-    return res
 
-def get_vlans(config):
+def get_bridges(config):
     """
-5. Вывести вланы, не участвующие в бриджах и в "ip addresses"
+4. ToDo: Исключить бриджы которые участвуют в "ip addresses"
+    Вывести бриджы без портов
+    Вывести бриджы с одним портом и эти одиночные порты
     """
+    class Res(list):
+        def __str__(self):
+            res = f'Empty bridges: {len(self[0])}. Bridges with single port: {len(self[1])}. Count free vlans: {len(self[2])}\n'
+            res += f'Empty bridges: {len(self[0])}\n'
+            res += "\n".join(self[0])
+
+            res += f'\n\nSingle bridges: {len(self[1])}\n'
+            res += "\n".join(self[1])
+
+            res += f'\n\nFree vlans: {len(self[2])}\n'
+            res += "\n".join(self[2])
+            return res
+
+    br_empty = set()
+    br_single = set()
+    int_single = set()
+
+    all_bridges = set(parse_section(regex_section.interface_bridge,config)) # получаем все бриджи из конфига
+    int_ip_addr = set(parse_section(regex_section.ip_address, config)) # получаем все порты на которых есть ip
+    br_without_ipaddr = all_bridges - int_ip_addr # исключаем бриджы на которых есть ip
+    bridge_dict = dict([(bridge, []) for bridge in br_without_ipaddr])  # формируем словарь из бриджей, пока без портов
+    bridge_ports = parse_section(regex_section.interface_bridge_port,config)  # получаем бриджы и порты из конфига
+    for bridge, port in bridge_ports:
+        if bridge in bridge_dict:
+            bridge_dict[bridge] += [port]
+
+    for bridge, ports in bridge_dict.items():
+        if not ports:
+            br_empty.add(bridge)
+        elif len(ports) == 1:
+            br_single.add(bridge)
+            if ports[0] not in int_ip_addr: # исключаем интерфейсы которые есть в "ip addresss"
+                int_single.add(ports[0])
+
+
+    # res = set()
+    # return Res(res)
+    return Res([br_empty,br_single,int_single])
+
+def get_free_vlans(config):
+    """
+5. DONE! ToDo: Вывести вланы, не участвующие в бриджах и в "ip addresses"
+    """
+    class Res(list):
+        def __str__(self):
+            res = f'Total vlans:{self[1]}. Vlans in ip addresses: {self[2]}. Vlans in bridges: {self[3]}.\n' \
+                  f'Count free vlans: {len(self[0])}\n'
+            res += "\n".join(self[0])
+            return res
+
     vlans = parse_section(regex_section.interface_vlan, config)  # получаем список всех влан
     int_ip_addr = parse_section(regex_section.ip_address, config)  # получаем список интерфейсов на которых есть ip
-    res = set(vlans) - set(int_ip_addr)
-    # ToDo: исключить вланы, участвующие в бриджах
-    return res
+    port_in_bridges = parse_section(regex_section.interface_bridge_port, config, reg_id=2)
+    # Done: исключить вланы, участвующие в бриджах
+
+    free_vlans = set(vlans) - set(int_ip_addr) - set(port_in_bridges)
+    return Res([free_vlans, len(vlans), len(int_ip_addr), len(port_in_bridges)])
+
 
 if __name__ == '__main__':
     ipfile = 'export_compact.txt.rsc'
+    file_tu = 'ip_from_address_plan.txt'
     with open(ipfile, encoding='ANSI') as file:
         config = file.read()
 
-    vlans = get_vlans(config)
-    print('Count vlans:', len(vlans))
-    pprint(vlans)
+    # vlans = get_free_vlans(config)
+    # print(vlans)
+
+    bridge = get_bridges(config)
+    print(bridge)
--- regex_example.py
+++ regex_example.py
@@ -5,44 +5,48 @@ from pprint import pprint
 
 regex_flash = r'\\\n *'  # удаление переносов из конфигурационного файла
 
-# формирование регулярного выражения для заданной секции для выборки из "export_compact"
-def new_regex_section(section):
-    return rf'\{section}\n([\s\S]+?)\n\/'
-
-
-def parse_section(section, config, reg_id=1):
-    """
-
-    :param section:
-    :param config:
-    :param reg_id: выбор регулярки с помощью которой парсить секцию
-    :return:
-    """
-    # pass
-    config = re.sub(regex_flash, '', config)  # удаление переносов из конфигурационного файла
-    section_config = re.findall(section[0], config)[0]
-    res = re.findall(section[reg_id], section_config)
-    return res
-
-
 # Выбор одноименной секции из export_compact
 sections = dict([
     ('interface_bridge', [r'add(?:.+)name=(.+?)(?: protocol-mode|\n)']),  # возвращает имя bridge
     ('interface_eoip', [r'add\b(?:.+?)local-address=((?:\d+\.){3}\d+)(?:.+?)remote-address=((?:\d+\.){3}\d+)']),
     # возвращает local-address, remote-address
-     # [r'add\b(?:.+?)local-address=((?:\d+\.){3}\d+)(?:.+?)name=(.+?) remote-address=((?:\d+\.){3}\d+)']),
+    # [r'add\b(?:.+?)local-address=((?:\d+\.){3}\d+)(?:.+?)name=(.+?) remote-address=((?:\d+\.){3}\d+)']),
     ('interface_vlan', [r'add(?:.+)name=(.+?)(?: vlan-id|\n)']),  # возвращает имя vlan
-    ('interface_bridge_port', [r'add\b(?:.+?)bridge=(.+)(?:.+?)interface=(.+?)\n']),  # возвращает bridge и interface
-    ('ppp_secret', [r'add(?:.+)remote-address=((?:\d+\.){3}\d+)(?: service|\n| )']), # возвращает remote-address
+    ('interface_bridge_port', [r'add\b(?:.+?)bridge=(.+)(?:.+?)interface=(.+?)\n',  # возвращает bridge и interface
+                               r'add\b(?:.+?)interface=(.+?)\n']),  # возвращает только interface
+    ('ppp_secret', [r'add(?:.+)remote-address=((?:\d+\.){3}\d+)(?: service|\n| )']),  # возвращает remote-address
     ('ip_address', [r'add(?:.+)interface=(.+?)(?: network|\n)',  # возвращает interface
-                    r'add address=((?:\d+\.){3}\d+)'])   # возвращает ip
+                    r'add address=((?:\d+\.){3}\d+)'])  # возвращает ip
 ])
+
+
+# формирование регулярного выражения для заданной секции для выборки из "export_compact"
+def new_regex_section(section):
+    return rf'\{section}\n([\s\S]+?)\n\/'
+
 
 Regex_sections = namedtuple('Regex_sections', sections)
 
 regex_section = Regex_sections._make([
-    [new_regex_section('/' + section.replace('_', ' '))] + sections[section] for section in Regex_sections._fields
-])
+    [new_regex_section('/' + section.replace('_', ' '))] + sections[section] for section in Regex_sections._fields])
+"""Итоговая структура regex_section:
+    regex_section.name_of_section: regexlist
+    regexlist - список регулярных выражаний для:
+    regexlist[0] - для выбора всей секции из конфигурационного файла
+    regexlist[1:] - для выбора нужных значений в зависимости от задачи
+"""
+
+
+def parse_section(section, config, reg_id=1):
+    """
+    :param reg_id: id регулярки из списка в regex_section, с помощью которой парсить секцию
+    """
+    # pass
+    config = re.sub(regex_flash, '', config)  # удаление переносов из конфигурационного файла
+    section_config = re.findall(section[0], config)[0]  #выбор всей секции из конфигурационного файла
+    res = re.findall(section[reg_id], section_config)  #выбор нужных значений в зависимости от reg_id
+    return res
+
 
 if __name__ == '__main__':
     ipfile = 'export_compact.txt.rsc'
